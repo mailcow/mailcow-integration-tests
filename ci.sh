e8feb71892acc4fee74b7d1a88487ace46b24126
zsan <zaksantanu@gmail.com>
2020-10-01 05:06:43 +0700

    Update ci.sh
    private domain?

--- ci.sh
+++ ci.sh
@@ -28,9 +28,9 @@ echo "ci_mailcow__api_key: $(openssl rand -hex 25)"  > /drone/src/group_vars/all
 echo "ci_mailcow__api_key_read_only: $(openssl rand -hex 25)"  > /drone/src/group_vars/all/drone_mailcow__api_key_read_only.yml
 echo "ci_mailcow__upload_results: true" > /drone/src/group_vars/all/mailcow__upload_results.yml
 
-domains[0]="010111010.xyz"
-domains[0]="4884884.xyz"
-domains[0]="8448448.xyz"
+#domains[0]="010111010.xyz"
+#domains[0]="4884884.xyz"
+domains[0]="yuli.shop"
 domains_size=${#domains[@]}
 domains_index=$(($RANDOM % $domains_size))
 
