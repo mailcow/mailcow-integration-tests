e8263c3a5678b44fc24d5c9c6b02961d95ad8f7e
ntimo <git@nowitzki.me>
2020-10-05 16:30:20 +0000

    Install nessary Ansible collections

--- ci-piprequierments.sh
+++ ci-piprequierments.sh
@@ -6,3 +6,7 @@
 pip3 install hcloud > /dev/null
 pip3 install jmespath > /dev/null
 pip3 install docker > /dev/null
+
+# Install Ansible
+ansible-galaxy collection install community.general > /dev/null
+ansible-galaxy collection install hetzner.hcloud > /dev/null
